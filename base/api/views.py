f8c27bc1e69b21203a58936192f6202364c206be
Bona-D <amdnaththanayake@std.appsc.sab.ac.lk>
2022-12-10 00:53:06 +0530

    After Long time push

--- base/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-def getRoutes(request):
-    routes = [
-        'Get /api/rooms',
-        'Get /api/:id'
-    ]
-    return JsonResponse(routes, safe=False)
